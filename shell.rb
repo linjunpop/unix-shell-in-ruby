639b6d61df3a1f90332010dee7432be5507388d2
Jun Lin <linjunpop@gmail.com>
2013-09-26 15:11:17 +0800

    Implemented 'cd' builtin command.

--- shell.rb
+++ shell.rb
@@ -1,16 +1,35 @@
 #!/usr/bin/env ruby
 
+require 'shellwords'
+
 class Shelly
+  BUILTINS = {
+    'cd' => ->(dir) { Dir.chdir(dir) }
+  }
+
   def run
-    print_prompt
-
-    $stdin.each_line do |line|
-      pid = fork {
-        exec line
-      }
-
-      Process.wait pid
+    loop do
       print_prompt
+
+      line = $stdin.gets
+
+      if line
+        line.strip!
+      else
+        exit
+      end
+
+      command, *arguments = line.shellsplit
+
+      if BUILTINS[command]
+        BUILTINS[command].call(*arguments)
+      else
+        pid = fork {
+          exec line
+        }
+
+        Process.wait pid
+      end
     end
   end
 
