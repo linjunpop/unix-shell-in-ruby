0742474101d20c569ffe709446a3d8f3ee0d97cc
Jun Lin <linjunpop@gmail.com>
2013-09-26 16:18:46 +0800

    Pipeline.

--- shirt.rb
+++ shirt.rb
@@ -19,16 +19,61 @@ class Shirt
 
       line = $stdin.gets.strip
 
-      command, *arguments = line.shellsplit
+      commands = split_on_pipes(line)
 
-      if BUILTINS[command]
-        BUILTINS[command].call(*arguments)
-      else
-        pid = fork { exec(command, *arguments) }
+      placeholder_in = $stdin
+      placeholder_out = $stdout
+      pipe = []
 
-        Process.wait pid
+      commands.each_with_index do |command, index|
+        program, *arguments = command.shellsplit
+
+        if builtin?(program)
+          BUILTINS[program].call(*arguments)
+        else
+          if index + 1 < commands.size
+            pipe = IO.pipe
+            placeholder_out = pipe.last
+          else
+            placeholder_out = $stdout
+          end
+
+          spawn_program(program, *arguments, placeholder_out, placeholder_in)
+
+          placeholder_out.close unless placeholder_out == $stdout
+          placeholder_in.close unless placeholder_in == $stdin
+          placeholder_in = pipe.first
+        end
+
+        Process.waitall
       end
     end
+  end
+
+  private
+
+  def split_on_pipes(line)
+    line.scan( /([^"'|]+)|["']([^"']*)["']/ ).flatten.compact
+  end
+
+  def builtin?(program)
+    !! BUILTINS[program]
+  end
+
+  def spawn_program(program, *arguments, placeholder_out, placeholder_in)
+    fork {
+      unless placeholder_out == $stdout
+        $stdout.reopen(placeholder_out)
+        placeholder_out.close
+      end
+
+      unless placeholder_in == $stdin
+        $stdin.reopen(placeholder_in)
+        placeholder_in.close
+      end
+
+      exec program, *arguments
+    }
   end
 end
 
